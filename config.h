b8812c4b37a0c062883e5395140f6e3820ac94c5
soziopath <a.j.schaeffler@googlemail.com>
2021-07-14 17:20:38 +0200

    changed font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=19:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 0;
 
